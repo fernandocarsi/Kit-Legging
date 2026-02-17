7fa3257ab3686b1dacddf3d365fb02ee8f00b9e3
Fernando <fernandocarsijf@gmail.com>
2026-02-16 21:50:47 -0300

    fix: resolve index.tsx merge conflict

--- index.tsx
+++ index.tsx
@@ -1,9 +1,4 @@
-
-<<<<<<< HEAD
-
-=======
 import '@angular/compiler';
->>>>>>> e2e38a68bf032da9f60bece63b838655e15aac5f
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './src/app.component';
 import { provideZonelessChangeDetection } from '@angular/core';
@@ -16,5 +11,3 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withInterceptors([mockBackendInterceptor])) 
   ]
 }).catch((err) => console.error(err));
-
-// AI Studio always uses an `index.tsx` file for all project types.
